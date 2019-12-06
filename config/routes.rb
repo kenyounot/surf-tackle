89a4378c9a730b22f40d45b17537fbc9c3f0c263
Kenneth Spatta <kenyounot@gmail.com>
2019-12-05 20:39:12 -0500

    added users registration with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :reviews
+  resources :brands
+  resources :items
+  devise_for :users, :controllers => {registrations: 'registrations'}
+
+  root to: 'application#welcome'
 end
