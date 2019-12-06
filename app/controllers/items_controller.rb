9e290c53b6a5658b550b1d89bc08944a2dfab951
Kenneth Spatta <kenyounot@gmail.com>
2019-12-05 21:29:05 -0500

    created index page to view all items made

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,10 @@ class ItemsController < ApplicationController
          end
     end
 
+    def index
+        @items = Item.all
+    end
+
     def show
         @item =  Item.find_by_id(params[:id])
     end
