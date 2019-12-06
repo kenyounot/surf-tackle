7eb7c7a4b53d1a58ae56cacf456f17386d6b3519
Kenneth Spatta <kenyounot@gmail.com>
2019-12-05 21:15:29 -0500

    added create item form and show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,27 @@
 class ItemsController < ApplicationController
+    def new
+        @item = Item.new
+        @item.build_brand
+    end
+
+    def create
+         @item = Item.new(item_params)
+
+         if @item.save
+            redirect_to item_path(@item)
+         else
+            render :new
+         end
+    end
+
+    def show
+        @item =  Item.find_by_id(params[:id])
+    end
+
+
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :price, :description, :brand_id, brand_attributes: [:name])
+    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :brand
+  has_many :reviews
+  has_many :users, through: :reviews
+
+  accepts_nested_attributes_for :brand
 end
