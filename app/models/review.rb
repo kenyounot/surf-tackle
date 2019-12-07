fe73ddeeecad40bce9363dcf83a5c12dbc9ca088
Kenneth Spatta <kenyounot@gmail.com>
2019-12-07 13:05:28 -0500

    created validations for review to allow only one review per user

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,6 @@ class Review < ApplicationRecord
 
   validates :title, presence: true
   validates :stars, numericality: { greater_than_or_equal_to: 0, less_than: 6 }
+
+  validates :item, uniqueness: {scope: :user, message: 'Can only review an item once.'}
 end
