27f88a1aa204b0a54fb2bd51940da5a916d52b31
Kenneth Spatta <kenyounot@gmail.com>
2019-12-09 00:15:24 -0500

    added some styling to home page

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,5 +7,5 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :stars, numericality: { greater_than_or_equal_to: 0, less_than: 6 }
 
-  validates :item, uniqueness: {scope: :user, message: 'Can only review an item once.'}
+  validates :item, uniqueness: {scope: :user, message: '- user can only review an item once.'}
 end
